45a52d00417bfa163a8b8552fc7c19df192aa605
jakewallin82 <jakewallin82@gmail.com>
2023-02-28 22:10:07 -0800

    Base code for OpenAI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import axios from "axios";
+import { Button, Col, Input, Slider, Typography, Row } from "antd";
+import React, { useState } from "react";
 import { Amplify } from "aws-amplify";
 import {
   AmplifyProvider,
   Authenticator,
-  Button,
   Flex,
   Image,
-  Text,
   View,
 } from "@aws-amplify/ui-react";
 import aws_exports from "./aws-exports";
@@ -15,9 +15,56 @@ import "@aws-amplify/ui-react/styles.css";
 import theme from "./theme";
 import logo from "./logo.svg";
 
+const { TextArea } = Input;
+
+const { Text } = Typography;
+
 Amplify.configure(aws_exports);
 
+const apiKey = "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";
+
+const client = axios.create({
+  headers: {
+    Authorization: "Bearer " + apiKey,
+  },
+});
+
+const params = {
+  prompt: "How are you?",
+  model: "text-davinci-003",
+  max_tokens: 10,
+  temperature: 0,
+};
+
 const App = () => {
+  const [payload, setPayload] = useState("");
+  const [gptResponse, setGptResponse] = useState("");
+  const [temperature, setTemperature] = useState(0);
+  const [maxTokens, setMaxTokens] = useState(100);
+
+  const handleSubmitPrompt = (): void => {
+    client
+      .post("https://api.openai.com/v1/completions", {
+        prompt: payload,
+        model: "text-davinci-003",
+        max_tokens: maxTokens,
+        temperature: temperature,
+      })
+      .then((result) => {
+        console.log(result.data.choices[0].text);
+        setGptResponse(result.data.choices[0].text);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const onTemperatureChange = (temp: number) => {
+    setTemperature(temp);
+  };
+  const onMaxTokensChange = (tokens: number) => {
+    setMaxTokens(tokens);
+  };
+
   return (
     <AmplifyProvider theme={theme}>
       <Authenticator>
@@ -31,24 +78,38 @@ const App = () => {
             gap="1rem"
             textAlign="center"
           >
-            <View width="100%">
-              <Image src={logo} alt="logo" width={240} height={240} />
-            </View>
-
-            {user && (
-              <View width="100%">
-                <Text>Hello {user.username}</Text>
-                <Button onClick={signOut}>
-                  <Text>Sign Out</Text>
-                </Button>
-              </View>
-            )}
-
-            <View width="100%">
-              <Text>
-                Edit <code>src/App.tsx</code> and save to reload.
-              </Text>
-            </View>
+            <Row>
+              <TextArea
+                placeholder="prompt"
+                onChange={(e) => setPayload(e.target.value)}
+                rows={4}
+              />
+            </Row>
+            <Row>
+              <Col span={8}>
+                <Slider
+                  min={0}
+                  max={1}
+                  onChange={onTemperatureChange}
+                  value={temperature}
+                  step={0.01}
+                />
+              </Col>
+              <Col span={8}>
+                <Slider
+                  min={100}
+                  max={1000}
+                  onChange={onMaxTokensChange}
+                  value={maxTokens}
+                />
+              </Col>
+              <Col span={8}>
+                <Button onClick={handleSubmitPrompt}>Submit Prompt</Button>
+              </Col>
+            </Row>
+            <Row>
+              <Text>{gptResponse}</Text>
+            </Row>
           </Flex>
         )}
       </Authenticator>
